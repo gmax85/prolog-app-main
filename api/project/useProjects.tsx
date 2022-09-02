0b3037b227153f62da92ffa4687ced1c435f0d6f
Max <gerasimovpost@gmail.com>
2022-09-02 22:14:10 +0400

    Uses separate project types file

--- api/project/useProjects.tsx
+++ api/project/useProjects.tsx
@@ -1,26 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-
-export enum ProjectLanguage {
-  react = "react",
-  node = "node",
-  python = "python",
-}
-
-export enum ProjectStatus {
-  info = "info",
-  warning = "warning",
-  error = "error",
-}
-
-export type Project = {
-  id: string;
-  name: string;
-  language: ProjectLanguage;
-  numIssues: number;
-  numEvents24h: number;
-  status: ProjectStatus;
-};
+import { Project } from "./project.types";
 
 async function getProject() {
   const { data } = await axios.get("http://prolog-api.profy.dev/project");
