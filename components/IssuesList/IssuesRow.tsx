4e66a437c9fff723241f22ff77998344aadc930d
Max <gerasimovpost@gmail.com>
2022-09-22 18:55:52 +0400

    Style issue row

--- components/IssuesList/IssuesRow.tsx
+++ components/IssuesList/IssuesRow.tsx
@@ -1,8 +1,8 @@
 import { IssueLevel } from "@api/issue";
 import type { Issue } from "@api/issue";
 import { ProjectLanguage } from "@api/project";
-import { Badge, BadgeColor } from "@components/Badge";
-import { space } from "@styles/theme";
+import { Badge, BadgeColor, BadgeSize } from "@components/Badge";
+import { color, space, textFont } from "@styles/theme";
 import capitalize from "lodash/capitalize";
 import styled from "styled-components";
 
@@ -17,36 +17,63 @@ const levelColors = {
   [IssueLevel.error]: BadgeColor.error,
 };
 
+const Row = styled.tr`
+  &:nth-child(2n) {
+    background: ${color("gray", 50)};
+  }
+`;
+
+const Cell = styled.td`
+  padding: ${space(4, 6)};
+  ${textFont("sm", "regular")}
+  color: ${color("gray", 500)};
+`;
+
+const IssueCell = styled(Cell)`
+  display: flex;
+  align-items: center;
+`;
+
 const LanguageIcon = styled.img`
-  margin-right: ${space(3)};
+Width: ${space(10)}
+margin-right: ${space(3)};
+`;
+
+const ErrorTypeAndMessage = styled.div`
+  color: ${color("gray", 900)};
+`;
+
+const ErrorType = styled.span`
+  ${textFont("sm", "regular")}
 `;
 
 export function IssueRow({ projectLanguage, issue }: IssueRowProps) {
   const { name, message, stack, level } = issue;
+  const firstLineOfStackTrace = stack.split("\n")[1];
   const numEvents = 100;
   const numUsers = 80;
   return (
-    <div>
-      <div>
+    <Row>
+      <IssueCell>
+        <LanguageIcon
+          src={`/icons/${projectLanguage}.svg`}
+          alt={projectLanguage}
+        />
         <div>
-          <LanguageIcon
-            src={`/icons/${projectLanguage}.svg`}
-            alt={projectLanguage}
-          />
-          <div>
-            <div>
-              <span>{name}</span>
-              {message}
-            </div>
-            <div>{stack.split("\n")[1]}</div>
-          </div>
+          <ErrorTypeAndMessage>
+            <ErrorType>{name}:&nbsp;</ErrorType>
+            {message}
+          </ErrorTypeAndMessage>
+          <div>{firstLineOfStackTrace}</div>
         </div>
-        <div>
-          <Badge color={levelColors[level]}>{capitalize(level)} </Badge>
-        </div>
-        <div>{numEvents}</div>
-        <div>{numUsers}</div>
-      </div>
-    </div>
+      </IssueCell>
+      <Cell>
+        <Badge color={levelColors[level]} size={BadgeSize.sm}>
+          {capitalize(level)}{" "}
+        </Badge>
+      </Cell>
+      <Cell>{numEvents}</Cell>
+      <Cell>{numUsers}</Cell>
+    </Row>
   );
 }
