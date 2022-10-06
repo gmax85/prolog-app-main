0d7ef46a64a01bf43e591bced1f262b458fd896c
Max <gerasimovpost@gmail.com>
2022-10-06 20:59:07 +0400

    Updated

--- cypress/integration/issue-list.spec.ts
+++ cypress/integration/issue-list.spec.ts
@@ -2,32 +2,21 @@ import mockIssues1 from "../fixtures/issues-page-1.json";
 import mockIssues2 from "../fixtures/issues-page-2.json";
 import mockIssues3 from "../fixtures/issues-page-3.json";
 
-describe("Issue List", () => {
+describe("Project List", () => {
   beforeEach(() => {
-    // setup request mocks
     cy.intercept("GET", "https://prolog-api.profy.dev/project", {
       fixture: "projects.json",
-    }).as("getProjects");
+    });
     cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=1", {
       fixture: "issues-page-1.json",
-    }).as("getIssues");
+    });
     cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=2", {
       fixture: "issues-page-2.json",
     });
     cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=3", {
       fixture: "issues-page-3.json",
     });
-
-    // open issues page
-    cy.visit(`http://localhost:3000/issues`);
-
-    // wait for request to resolve
-    // cy.wait("@getProjects");
-    cy.wait("@getIssues");
-
-    // set button aliases
-    cy.get("button", { timeout: 10000 }).contains("Previous").as("prev-button");
-    cy.get("button").contains("Next").as("next-button");
+    cy.visit("http://localhost:3000/issues");
   });
 
   context("desktop resolution", () => {
@@ -36,11 +25,13 @@ describe("Issue List", () => {
     });
 
     it("renders the issues", () => {
-      cy.get("main")
-        .find("tbody")
-        .find("tr")
+      cy.findByRole("main")
+        .findAllByRole("row")
         .each(($el, index) => {
-          const issue = mockIssues1.items[index];
+          // skip the header row
+          if (index === 0) return;
+
+          const issue = mockIssues1.items[index - 1];
           const firstLineOfStackTrace = issue.stack.split("\n")[1].trim();
           cy.wrap($el).contains(issue.name);
           cy.wrap($el).contains(issue.message);
@@ -52,33 +43,44 @@ describe("Issue List", () => {
     it("paginates the data", () => {
       // test first page
       cy.contains("Page 1 of 3");
-      cy.get("@prev-button").should("have.attr", "disabled");
+      cy.findByRole("button", { name: "Previous" }).should(
+        "have.attr",
+        "disabled"
+      );
 
       // test navigation to second page
-      cy.get("@next-button").click();
-      cy.get("@prev-button").should("not.have.attr", "disabled");
+      cy.findByRole("button", { name: "Next" }).click();
+      cy.findByRole("button", { name: "Previous" }).should(
+        "not.have.attr",
+        "disabled"
+      );
       cy.contains("Page 2 of 3");
       cy.get("tbody tr:first").contains(mockIssues2.items[0].message);
 
       // test navigation to third and last page
-      cy.get("@next-button").click();
-      cy.get("@next-button").should("have.attr", "disabled");
+      cy.findByRole("button", { name: "Next" }).click();
+      cy.findByRole("button", { name: "Next" }).should("have.attr", "disabled");
       cy.contains("Page 3 of 3");
       cy.get("tbody tr:first").contains(mockIssues3.items[0].message);
 
       // test navigation back to second page
-      cy.get("@prev-button").click();
-      cy.get("@next-button").should("not.have.attr", "disabled");
+      cy.findByRole("button", { name: "Previous" }).click();
+      cy.findByRole("button", { name: "Next" }).should(
+        "not.have.attr",
+        "disabled"
+      );
       cy.contains("Page 2 of 3");
       cy.get("tbody tr:first").contains(mockIssues2.items[0].message);
     });
 
-    it("persists page after reload", () => {
-      cy.get("@next-button").click();
-      cy.contains("Page 1 of 3");
+    it("perists page after reload", () => {
+      cy.findByRole("button", { name: "Next" }).click();
+      cy.contains("Page 2 of 3");
 
       cy.reload();
-      cy.contains("Page 1 of 3");
+      cy.contains("Page 2 of 3");
     });
   });
 });
+
+export { };
