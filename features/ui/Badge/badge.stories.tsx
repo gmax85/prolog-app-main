69e12698c88a3154a0046e2852c2a99c8c8fab40
Max <gerasimovpost@gmail.com>
2022-10-19 19:25:06 +0400

    Rename to lower case

--- features/ui/Badge/badge.stories.tsx
+++ features/ui/Badge/badge.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Badge, BadgeColor, BadgeSize } from "./abadge";
+import { Badge, BadgeColor, BadgeSize } from "./badge";
 
 export default {
   title: "UI/Badge",
