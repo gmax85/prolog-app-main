a6fae20993b54b4af31ada03e85df9438e75990a
Max <gerasimovpost@gmail.com>
2022-10-19 19:22:51 +0400

    Temporarily rename files

--- features/ui/Badge/abadge.stories.tsx
+++ features/ui/Badge/abadge.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Badge, BadgeColor, BadgeSize } from "./badge";
+import { Badge, BadgeColor, BadgeSize } from "./abadge";
 
 export default {
   title: "UI/Badge",
